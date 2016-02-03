c5e8ad99c9a78d5602ec5d69d23c382b0ae0ffe1
Dalmir Silva <dalmirdasilva@gmail.com>
2016-02-02 22:52:25 -0200

    added few examples

--- AccelerometerMMA8451/examples/transient_detection_sending_via_serial/transient_detection_sending_via_serial.ino
+++ AccelerometerMMA8451/examples/transient_detection_sending_via_serial/transient_detection_sending_via_serial.ino
@@ -4,31 +4,16 @@
 #include <Accelerometer.h>
 #include <AccelerometerMMA8451.h>
 
-#include <MAX7219Driver.h>
-#include <LedMatrixDriver.h>
-#include <LedMatrixMAX7219Driver.h>
-
-#include <SoftwareSerial.h>
-
-#define DEBOUNCE_INPUT_PIN A2
-#define THRESHOLD_INPUT_PIN A3
-#define MIN_TIME_BETWEEN_STEPS 1000
-
-MAX7219Driver driver(10, 11, 12);
-LedMatrixMAX7219Driver matrix(&driver, 8, 8);
-unsigned long resistor, lastStepTime;
-unsigned char threshold = 0x08, debounce = 0x05;
-
-/**
- * Embedded Transient Detection Using the MMA8451
- */
-
 AccelerometerMMA8451 acc(0);
+SoftwareSerial serial(3, 4);
 volatile bool ready = false;
 unsigned char buf[6];
+message[]
+        unsigned char message[] = {0xaa, 0x1, 0x2, 0x3, 0x4, 0x00, 0x06, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0x00, 0x01, 0xbb};
+serial.write(message);
 
 void processXYZ(unsigned char* buf) {
-    char axis[] = { 'x', 'y', 'z' };
+    char axis[] = {'x', 'y', 'z'};
     for (int i = 0; i < 6; i += 2) {
         Serial.print(axis[i / 2]);
         Serial.print(": ");
@@ -46,18 +31,9 @@ void processTransientData(AccelerometerMMA8451::TRANSIENT_SRCbits transientSourc
     Serial.print(" py: ");
     Serial.println(transientSource.Y_TRANS_POL);
     Serial.print("z: ");
-    Serial.print(transientSource.ZTRANSE, HEX);
+    Serial.print(transientSource.ZTRANSE, HEX);   
     Serial.print(" pz: ");
     Serial.println(transientSource.Z_TRANS_POL);
-
-    unsigned long now = millis();
-    if (now - MIN_TIME_BETWEEN_STEPS > lastStepTime && (transientSource.X_TRANS_POL + transientSource.Y_TRANS_POL + transientSource.Z_TRANS_POL) == 0) {
-        lastStepTime = now;
-        matrix.fill();
-        delay(100);
-        matrix.clear();
-        Serial.println("STEP");
-    }
 }
 
 void isr() {
@@ -71,27 +47,21 @@ void setup() {
     Serial.begin(9600);
 
     Serial.println("Setup...");
-    
-    lastStepTime = millis();
-
-    pinMode(THRESHOLD_INPUT_PIN, INPUT);
-
-    matrix.clear();
 
     // Put the part into Standby Mode
     acc.standby();
 
     // Set the data rate to 50 Hz (for example, but can choose any sample rate). 
-    acc.setOutputDataRate(AccelerometerMMA8451::ODR_100HZ_10_MS);
+    acc.setOutputDataRate(AccelerometerMMA8451::ODR_50HZ_20_MS);
 
     // This will enable the transient detection.
-    acc.setTransientDetection(true, 0x07, 0x00);
+    acc.setTransientDetection(true, 0x7f, 0x00);
 
     // Set the transient threshold.
-    acc.setTransientThreshold(true, threshold);
+    acc.setTransientThreshold(true, 0x0f);
 
     // Set the debounce counter
-    acc.writeRegister(AccelerometerMMA8451::TRANSIENT_COUNT, debounce);
+    acc.writeRegister(AccelerometerMMA8451::TRANSIENT_COUNT, 0x01);
 
     // Register 0x2D, Control Register 4 configures all embedded features for interrupt detection.
     // To set this device up to run an interrupt service routine: 
@@ -112,23 +82,6 @@ void setup() {
 }
 
 void loop() {
-    resistor = analogRead(THRESHOLD_INPUT_PIN);
-    unsigned char newThreshold = map(resistor, 0, 1023, 0, 0x7f) & 0xff;
-    if (abs(newThreshold - threshold) > 4) {
-        acc.setTransientThreshold(true, newThreshold);
-        threshold = newThreshold;
-        Serial.print("new threshold: ");
-        Serial.println(threshold, HEX);
-    }
-
-    resistor = analogRead(DEBOUNCE_INPUT_PIN);
-    unsigned char newDebounce = map(resistor, 0, 1023, 0, 255) & 0xff;
-    if (abs(newDebounce - debounce) > 4) {
-        acc.writeRegister(AccelerometerMMA8451::TRANSIENT_COUNT, newDebounce);
-        debounce = newDebounce;
-        Serial.print("new debounce: ");
-        Serial.println(debounce, HEX);
-    }
 
     if (ready) {
 
@@ -146,7 +99,8 @@ void loop() {
         AccelerometerMMA8451::INT_SOURCEbits source;
 
         source.value = acc.readRegister(AccelerometerMMA8451::INT_SOURCE);
-
+        
+        
         Serial.print("source: ");
         Serial.println(source.value, HEX);
 
@@ -170,7 +124,7 @@ void loop() {
 
             // Puts the values.
             processXYZ(buf);
-
+            
             processTransientData(transientSource);
         }
     }
