2b9f3be1d3ec30226e633df6b12afe562d4ce097
Dalmir da Silva <dalmirdasilva@gmail.com>
2016-05-18 13:18:58 -0300

    improve examples

--- AccelerometerMPU9250/examples/plotting/plotting.ino
+++ AccelerometerMPU9250/examples/plotting/plotting.ino
@@ -6,19 +6,31 @@
 #include <AccelerometerMPU9250.h>
 
 AccelerometerMPU9250 acc(0);
+volatile bool ready = false;
 unsigned char buf[6];
+unsigned long now;
+
+void isr() {
+    ready = true;
+}
 
 void setup() {
     Serial.begin(9600);
-    acc.setFullScaleRange(AccelerometerMPU9250::FS_SEL_8G);
+    acc.enableAxis(AccelerometerMPU9250::AXIS_XYZ);
+    acc.setLowPassFilter(true, AccelerometerMPU9250::DR_184_5_80_250_1);
+    acc.enableInterrupt(AccelerometerMPU9250::WOM_EN);
+    acc.setWakeOnMotionThreshold(0x30);
+    acc.setOutputDataRate(AccelerometerMPU9250::ODR_250HZ);
+    attachInterrupt(0, isr, FALLING);
 }
 
 void loop() {
     acc.readXYZ(buf);
     Serial.write(0xaa);
     Serial.write(buf, 6);
-    Serial.write(0x00);
-    unsigned long now = millis();
+    Serial.write(ready ? 0x01 : 0x00);
+    now = millis();
     Serial.write((unsigned char *) &now, 4);
     Serial.write(0xbb);
+    ready = false;
 }
